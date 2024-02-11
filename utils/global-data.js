e9b7c8b483367d753ca525527330b486ed1453ee
Haider Aftab <121552824+Haidercodehach@users.noreply.github.com>
2024-02-11 16:01:16 +0500

    changing globle-data

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,16 @@
 export const getGlobalData = () => {
+  var currentDate = new Date();
+  // Get the full year from the Date object
+  var currentYear = currentDate.getFullYear();
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Hayyan Studio';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Game Development is my passion';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'All rights reserved.'+ currentYear;
 
   return {
     name,
